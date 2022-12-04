6c0bbe00ee575a77fa717c078491fb5fde442cba
Paolo D'Amico <paolodamico@users.noreply.github.com>
2022-12-04 10:33:09 +0530

    comments

--- src/lib/fetch.ts
+++ src/lib/fetch.ts
@@ -6,6 +6,10 @@ declare global {
 	var cleanFetch: Fetch
 }
 
+/**
+ * Uses `fetch` from a clean iFrame to prevent tampering with the global fetch object that may leak user data.
+ * @returns 
+ */
 const getCleanFetch = () => {
 	const iframe = document.createElement('iframe')
 	document.body.appendChild(iframe)
