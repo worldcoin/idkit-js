aefcfdb1915b84a2d205e5e4f7f3633c32e3f622
Miguel Piedrafita <github@miguelpiedrafita.com>
2022-12-23 19:23:05 +0100

    add QR

---
+++ idkit/src/components/QRCode.tsx
@@ -0,0 +1,59 @@
+import { classNames } from '@/lib/utils'
+import { useEffect, useRef } from 'react'
+import QRCodeStyling from 'qr-code-styling-new'
+import { renderToString } from 'react-dom/server'
+import WorldcoinLogomark from './Icons/WorldcoinLogomark'
+
+const logoB64 = window.btoa(renderToString(<WorldcoinLogomark style={{ color: '#6445DD' }} />))
+
+const qrcode = new QRCodeStyling({
+	width: 320,
+	height: 320,
+	type: 'svg',
+	image: `data:image/svg+xml;base64,${logoB64}`,
+	cornersSquareOptions: {
+		type: 'extra-rounded',
+	},
+	cornersDotOptions: {
+		type: 'dot',
+	},
+	dotsOptions: {
+		color: 'currentColor',
+		type: 'extra-rounded',
+	},
+	backgroundOptions: {
+		color: 'transparent',
+	},
+	imageOptions: {
+		margin: 16,
+		hideBackgroundDots: true,
+	},
+})
+
+type Props = {
+	data: string
+	className?: string
+}
+
+export const Qrcode = ({ data, className = '' }: Props): JSX.Element => {
+	const ref = useRef<HTMLDivElement | null>(null)
+
+	useEffect(() => {
+		if (!ref.current) return
+
+		qrcode.append(ref.current)
+	}, [])
+
+	useEffect(() => {
+		qrcode.update({ data })
+	}, [data])
+	return (
+		<div
+			ref={ref}
+			className={classNames(
+				className,
+				'relative z-0 flex items-center justify-center rounded border box-border border-white/10 dark:border-black/10 [&_*]:text-black dark:[&_*]:text-white'
+			)}
+		/>
+	)
+}
