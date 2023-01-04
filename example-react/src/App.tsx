e16e8c93b8196bf766288b7a25c3e70883d0e465
Paolo D'Amico <paolodamico@users.noreply.github.com>
2023-01-04 09:59:24 -0800

    demo signal

--- example-react/src/App.tsx
+++ example-react/src/App.tsx
@@ -16,11 +16,7 @@ function App() {
 			className="App"
 			style={{ minHeight: "100vh", display: "flex", justifyContent: "center", alignItems: "center" }}
 		>
-			<IDKitWidget
-				actionId="wid_staging_f56691ad61aaf0021ab317e29cf11820"
-				signal="my_signal"
-				onVerification={handleProof}
-			>
+			<IDKitWidget actionId="wid_staging_1234" signal="my_signal" onVerification={handleProof}>
 				{({ open }) => <button onClick={open}>Click me</button>}
 			</IDKitWidget>
 		</div>
