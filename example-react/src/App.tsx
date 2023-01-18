c2d2075888a8d957afeece5b1819e31a31a641b6
Miguel Piedrafita <github@miguelpiedrafita.com>
2023-01-18 15:00:27 +0100

    chore(release): 0.2.1 (#38)
    * update posthog-js-lite version
    
    * update examples to 0.2.0
    
    * chore(release): 0.2.1

--- example-react/src/App.tsx
+++ example-react/src/App.tsx
@@ -2,13 +2,15 @@ import { IDKitWidget, ISuccessResult } from "@worldcoin/idkit";
 
 function App() {
 	const handleProof = (result: ISuccessResult) => {
-		console.log(result);
-		const promise = new Promise<void>((resolve, reject) => {
+		return new Promise<void>((resolve) => {
 			setTimeout(() => resolve(), 3000);
 			// NOTE: Example of how to decline the verification request and show an error message to the user
 			//setTimeout(() => reject({ message: "This phone number has already been used!" }), 3000);
 		});
-		return promise;
+	};
+
+	const onSuccess = (result: ISuccessResult) => {
+		console.log(result);
 	};
 
 	return (
@@ -16,7 +18,12 @@ function App() {
 			className="App"
 			style={{ minHeight: "100vh", display: "flex", justifyContent: "center", alignItems: "center" }}
 		>
-			<IDKitWidget actionId="wid_staging_1234" signal="my_signal" handleVerify={handleProof}>
+			<IDKitWidget
+				signal="my_signal"
+				onSuccess={onSuccess}
+				handleVerify={handleProof}
+				actionId="wid_staging_1234"
+			>
 				{({ open }) => <button onClick={open}>Click me</button>}
 			</IDKitWidget>
 		</div>
