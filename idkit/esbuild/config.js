7ab3cd9c31d952bea8376d0750d3c72bf071144c
Max Petretta <git@maxpetretta.com>
2023-01-19 12:28:29 +0100

    Moved project_id to .env file

--- idkit/esbuild/config.js
+++ idkit/esbuild/config.js
@@ -1,10 +1,25 @@
 import fs from 'fs'
+import 'dotenv/config'
 import { createRequire } from 'node:module'
 import { transformFileAsync as babelTransformFileAsync } from '@babel/core'
 import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
+
+// import * as dotenv from 'dotenv'
+// dotenv.config()
+
 const require = createRequire(import.meta.url)
 
 const packageJson = require('../package.json')
+
+// console.log(process.env)
+
+if (!process.env.WALLETCONNECT_PID) {
+	console.error(
+		'\x1b[1m\x1b[5m\x1b[31mWALLETCONNECT_PID\x1b[0m environment variable is required to build this project'
+	)
+	console.error('Set one either explicitly or create an .env file (see .env.sample)')
+	process.exit(1)
+}
 
 /**
  * @type {import('esbuild').Plugin}
@@ -48,6 +63,7 @@ export default /** @type {import('esbuild').BuildOptions} */ ({
 	define: {
 		global: 'globalThis',
 		IDKitVersion: JSON.stringify(packageJson.version),
+		'process.env.WALLETCONNECT_PID': `"${process.env.WALLETCONNECT_PID}"`,
 	},
 	entryPoints: [require.resolve('../src/index.ts')],
 	globalName: 'IDKit',
