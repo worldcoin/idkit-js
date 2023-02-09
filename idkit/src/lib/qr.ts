7964b5a0a6210f5727e75f587a2bf66387278534
Paolo D'Amico <paolodamico@users.noreply.github.com>
2023-02-09 12:59:54 +0100

    fix QR

--- idkit/src/lib/qr.ts
+++ idkit/src/lib/qr.ts
@@ -15,10 +15,12 @@ export function buildQRData(uri: string, returnUrl?: string): string {
 	const relay = uri.match(relayRegex)
 	const key = uri.match(keyRegex)
 
-	if (topic) result.searchParams.append('t', topic[0])
-	if (version) result.searchParams.append('v', version[0])
-	if (relay) result.searchParams.append('p', relay[0])
-	if (key) result.searchParams.append('k', key[0])
+	console.log(uri, topic, version, relay, key)
+
+	if (topic) result.searchParams.append('t', topic[1])
+	if (version) result.searchParams.append('v', version[1])
+	if (relay) result.searchParams.append('p', relay[1])
+	if (key) result.searchParams.append('k', key[1])
 
 	// returnUrl optionally instructs the WLD app how to return to the website after the verification is complete (intended for mobile only).
 	if (returnUrl) result.searchParams.append('r', returnUrl)
