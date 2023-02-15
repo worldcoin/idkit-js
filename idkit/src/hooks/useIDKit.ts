3c98d79ff71ec577cccc240d7f2c76310bcc9d0f
Miguel Piedrafita <github@miguelpiedrafita.com>
2023-02-15 02:47:18 +0000

    fix: make hook complimentary to component

--- idkit/src/hooks/useIDKit.ts
+++ idkit/src/hooks/useIDKit.ts
@@ -2,13 +2,25 @@ import { useEffect } from 'react'
 import useIDKitStore from '@/store/idkit'
 import type { Config } from '@/types/config'
 import { ConfigSource } from '@/types/config'
+import type { IDKitStore } from '@/store/idkit'
 
-const useIDKit = (options: Config) => {
-	const { open, onOpenChange, setOptions } = useIDKitStore()
+type HookConfig = Pick<Config, 'handleVerify' | 'onSuccess'>
+
+const getStore = ({ open, onOpenChange, addSuccessCallback, addVerificationCallback }: IDKitStore) => ({
+	open,
+	onOpenChange,
+	addSuccessCallback,
+	addVerificationCallback,
+})
+
+const useIDKit = (options: HookConfig) => {
+	const { open, onOpenChange, addSuccessCallback, addVerificationCallback } = useIDKitStore(getStore)
 
 	useEffect(() => {
-		setOptions(options, ConfigSource.HOOK)
-	}, [setOptions, options])
+		if (options.onSuccess) addSuccessCallback(options.onSuccess, ConfigSource.HOOK)
+		if (options.handleVerify) addVerificationCallback(options.handleVerify, ConfigSource.HOOK)
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [options])
 
 	return { open, setOpen: onOpenChange }
 }
