6fd0106a2e63f8043da335acdfea9ff2f9ae5b5f
Paolo D'Amico <paolodamico@users.noreply.github.com>
2023-02-20 15:44:51 -0800

    feat: export main hashing functions

--- idkit/src/index.ts
+++ idkit/src/index.ts
@@ -1,7 +1,8 @@
 import useIDKit from './hooks/useIDKit'
 import IDKitWidget from '@/components/IDKitWidget'
 import type { WidgetProps, Config } from '@/types/config'
+import { worldIDHash, validateABILikeEncoding } from './lib/hashing'
 import type { ISuccessResult, SignalType, PhoneSignalProof, OrbSignalProof } from '@/types'
 
-export { IDKitWidget, useIDKit }
+export { IDKitWidget, useIDKit, worldIDHash, validateABILikeEncoding }
 export type { ISuccessResult, Config, SignalType, PhoneSignalProof, OrbSignalProof, WidgetProps }
