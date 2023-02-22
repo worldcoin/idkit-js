dd06ab73fa5662584b5faa3dd8bb01e306d970af
Paolo D'Amico <paolodamico@users.noreply.github.com>
2023-02-21 17:52:23 -0800

    fix: move internal functions to main

--- idkit/src/index.ts
+++ idkit/src/index.ts
@@ -3,6 +3,14 @@ import type { ISuccessResult } from '@/types'
 import { solidityEncode } from './lib/hashing'
 import IDKitWidget from '@/components/IDKitWidget'
 import type { WidgetProps, Config } from '@/types/config'
+import { hashToField, validateABILikeEncoding, generateExternalNullifier } from './lib/hashing'
 
-export { IDKitWidget, useIDKit, solidityEncode }
+// NOTE: For internal use in other Worldcoin packages
+const internal = {
+	hashToField,
+	validateABILikeEncoding,
+	generateExternalNullifier,
+}
+
+export { IDKitWidget, useIDKit, solidityEncode, internal }
 export type { ISuccessResult, Config, WidgetProps }
