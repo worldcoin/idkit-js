3b656ae1582363a8972a2dcdaf95dd740236d37d
Miguel Piedrafita <github@miguelpiedrafita.com>
2023-02-21 16:16:01 -0800

    wip

--- idkit/src/index.ts
+++ idkit/src/index.ts
@@ -1,8 +1,8 @@
 import useIDKit from './hooks/useIDKit'
+import type { ISuccessResult } from '@/types'
 import { solidityEncode } from './lib/hashing'
 import IDKitWidget from '@/components/IDKitWidget'
-import type { ISuccessResult, SignalType } from '@/types'
 import type { WidgetProps, Config } from '@/types/config'
 
 export { IDKitWidget, useIDKit, solidityEncode }
-export type { ISuccessResult, Config, SignalType, WidgetProps }
+export type { ISuccessResult, Config, WidgetProps }
