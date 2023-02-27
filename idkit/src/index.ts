745cbaea156f38402ee5eec5fd447a414ff8ac08
Miguel Piedrafita <github@miguelpiedrafita.com>
2023-02-26 16:51:39 -0800

    chore: export app connection logic (#77)

--- idkit/src/index.ts
+++ idkit/src/index.ts
@@ -1,8 +1,11 @@
 import useIDKit from './hooks/useIDKit'
+import QRCode from './components/QRCode'
 import { solidityEncode } from './lib/hashing'
 import IDKitWidget from '@/components/IDKitWidget'
+import useAppConnection from '@/services/walletconnect'
 import type { WidgetProps, Config } from '@/types/config'
 import type { ISuccessResult, CredentialType } from '@/types'
+import { VerificationState, AppErrorCodes } from './types/app'
 import SignInWithWorldID from './components/SignInWithWorldID'
 import { hashToField, validateABILikeEncoding, generateExternalNullifier } from './lib/hashing'
 
@@ -11,6 +14,10 @@ const internal = {
 	hashToField,
 	validateABILikeEncoding,
 	generateExternalNullifier,
+	QRCode,
+	useAppConnection,
+	VerificationState,
+	AppErrorCodes,
 }
 
 export { IDKitWidget, useIDKit, solidityEncode, internal, SignInWithWorldID }
