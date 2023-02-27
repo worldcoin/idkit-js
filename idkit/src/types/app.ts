d6e196c83864943dae32b65a046be8bbe1afa7c6
Max Petretta <git@maxpetretta.com>
2023-02-27 13:42:52 -0800

    fix: remove unused error codes

--- idkit/src/types/app.ts
+++ idkit/src/types/app.ts
@@ -2,8 +2,6 @@ export enum AppErrorCodes {
 	ConnectionFailed = 'connection_failed',
 	VerificationRejected = 'verification_rejected',
 	AlreadySigned = 'already_signed',
-	InvalidActionID = 'invalid_action_id',
-	InvalidSignal = 'invalid_signal',
 	UnexpectedResponse = 'unexpected_response',
 	GenericError = 'generic_error',
 }
