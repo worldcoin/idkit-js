ce9007ddeed4be29ca09f29586e7229a5878be47
Paolo D'Amico <paolodamico@users.noreply.github.com>
2023-04-11 14:31:33 +0900

    config

--- idkit/jest.config.ts
+++ idkit/jest.config.ts
@@ -3,6 +3,10 @@
  * https://jestjs.io/docs/configuration
  */
 
+import type { PackageJson } from 'type-fest'
+import * as packageJson from './package.json'
+const { version }: Pick<PackageJson, 'version'> = packageJson
+
 export default {
 	// Automatically clear mock calls, instances, contexts and results before every test
 	clearMocks: true,
@@ -23,5 +27,8 @@ export default {
 				sourcemap: true,
 			},
 		],
+	},
+	globals: {
+		IDKitVersion: version,
 	},
 }
