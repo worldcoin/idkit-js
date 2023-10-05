dfa532c6bd5ecc9dc0e2733a25da879732b2206a
Miguel Piedrafita <github@miguel.build>
2023-10-05 20:29:53 +0200

    simplify encoding/decoding fn

--- packages/core/src/lib/utils.ts
+++ packages/core/src/lib/utils.ts
@@ -1,21 +1,7 @@
 export const buffer_encode = (buffer: ArrayBuffer): string => {
-	let encoded = ''
-
-	const bytes = new Uint8Array(buffer)
-	for (let i = 0; i < bytes.byteLength; i++) {
-		encoded += String.fromCharCode(bytes[i])
-	}
-
-	return Buffer.from(encoded).toString('base64')
+	return Buffer.from(buffer).toString('base64')
 }
 
 export const buffer_decode = (encoded: string): ArrayBuffer => {
-	const byteCharacters = Buffer.from(encoded, 'base64').toString('utf-8')
-
-	const byteNumbers = new Array(byteCharacters.length)
-	for (let i = 0; i < byteCharacters.length; i++) {
-		byteNumbers[i] = byteCharacters.charCodeAt(i)
-	}
-
-	return new Uint8Array(byteNumbers)
+	return Buffer.from(encoded, 'base64')
 }
