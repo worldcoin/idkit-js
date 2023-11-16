a89b0d57826a94b57a5dc3e42b6efe3a0e00d154
Paolo <149602456+pdtfh@users.noreply.github.com>
2023-11-15 20:05:10 -0800

    remove telemetry

--- packages/react/src/types/config.ts
+++ packages/react/src/types/config.ts
@@ -12,8 +12,6 @@ export type WidgetConfig = {
 	autoClose?: boolean
 	/** The theme to apply to the widget's UI. Defaults to `light`. */
 	theme?: 'dark' | 'light'
-	/** Whether opt-in telemetry is enabled. Very few events are sent, with no PII to help improve the project. Defaults to `false`. */
-	enableTelemetry?: boolean
 	/** Function to trigger when verification is successful. Should receive a single parameter of type `ISuccessResult` which contains the proof details. */
 	onSuccess: CallbackFn<ISuccessResult>
 	/** Called after the proof is returned from the World App, but before showing the success screen. Throwing in this screen will show the user a custom error. Used to perform additional validation when needed. */
