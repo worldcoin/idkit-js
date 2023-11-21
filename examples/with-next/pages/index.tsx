20630ab572529b0d698e59e4ad99ca0d66570785
Paolo <149602456+pdtfh@users.noreply.github.com>
2023-11-21 11:28:54 -0600

    Update index.tsx

--- examples/with-next/pages/index.tsx
+++ examples/with-next/pages/index.tsx
@@ -6,7 +6,7 @@ const Home = () => (
 		signal="test_signal"
 		onError={error => console.log(error)}
 		onSuccess={response => console.log(response)}
-		app_id="wid_staging_a8d860d5b3450f05ae09e8f4aa935b90"
+		app_id="app_staging_45068dca85829d2fd90e2dd6f0bff997"
 		bridge_url="https://wallet-bridge.stage-crypto.worldcoin.org"
 	>
 		{({ open }) => <button onClick={open}>Open IDKit</button>}
