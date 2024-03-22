eecb17485fac0b13a344aef2ecca748a97f0b093
0xPenryn <ian.dilick@toolsforhumanity.com>
2024-03-22 17:30:01 -0500

    feat: nextjs example use verifyCloudProof
    non-functional until dev portal v2 api is live, but can test by modifying verifyCloudProof to use v1 api and rebuilding

---
+++ examples/with-next/pages/api/verify.ts
@@ -0,0 +1,12 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { verifyCloudProof, IVerifyResponse } from '@worldcoin/idkit'
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+	if (req.method === 'POST') {
+		const { proof, app_id, action, signal } = req.body
+		const response = (await verifyCloudProof(proof, app_id, action, signal)) as IVerifyResponse
+		res.status(response.success ? 200 : 400).json(response)
+	} else {
+		res.status(405).json({ message: 'Method Not Allowed' })
+	}
+}
