470c7a925c1f2521da61b237f4963364675a3461
0xPenryn <ian.dilick@toolsforhumanity.com>
2024-03-22 17:27:25 -0500

    feat: verifyCloudProof

---
+++ packages/core/src/lib/backend.ts
@@ -0,0 +1,34 @@
+import { hashToField } from './hashing'
+import type { ISuccessResult } from '../types'
+
+export interface IVerifyResponse {
+	success: boolean
+	code?: string
+	detail?: string
+	attribute?: string | null
+}
+
+export async function verifyCloudProof(
+	proof: ISuccessResult,
+	app_id: `app_${string}`,
+	action: string,
+	signal?: string
+): Promise<IVerifyResponse> {
+	const response = await fetch(`https://developer.worldcoin.org/api/v2/verify/${app_id}`, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify({
+			...proof,
+			action,
+			signal_hash: hashToField(signal ?? '').digest,
+		}),
+	})
+
+	if (response.ok) {
+		return { success: true }
+	} else {
+		return { success: false, ...(await response.json()) } as IVerifyResponse
+	}
+}
