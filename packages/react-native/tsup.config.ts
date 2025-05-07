7ac51628409a3786575aac1c5b251ef96f37d4cb
Takis Kakalis <80459599+Takaros999@users.noreply.github.com>
2025-05-08 01:11:31 +0300

    Remove todo comment

--- packages/react-native/tsup.config.ts
+++ packages/react-native/tsup.config.ts
@@ -5,7 +5,6 @@ export default defineConfig({
 	clean: true,
 	outDir: 'build',
 	format: ['esm', 'cjs'],
-	// TODO: @Takaros999
 	external: ['zustand', 'ox'],
 	entry: ['src/index.ts'],
 	define: { 'process.env.NODE_ENV': '"production"' },
