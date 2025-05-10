b908c12fbce0e10a6b166c54c75478b4bbb41e4d
Takis Kakalis <80459599+Takaros999@users.noreply.github.com>
2025-05-10 13:36:35 +0300

    feat: Add expo example for idkit react native (#333)
    * Add with-expo example
    
    * Minor fixes

---
+++ examples/with-expo/app/_layout.tsx
@@ -0,0 +1,39 @@
+import { Stack } from 'expo-router'
+import * as Linking from 'expo-linking'
+import React, { useEffect } from 'react'
+import { StatusBar } from 'expo-status-bar'
+import { useColorScheme } from 'react-native'
+
+// Import quick crypto to polyfill
+import '../app/polyfills'
+
+export default function RootLayout() {
+	const colorScheme = useColorScheme()
+
+	useEffect(() => {
+		const handleDeepLink = (event: Linking.EventType) => {
+			const { path } = Linking.parse(event.url)
+			if (path === '') {
+				// Do nothing, just acknowledge it
+			}
+		}
+
+		// For cold starts
+		Linking.getInitialURL().then(url => {
+			if (url) {
+				handleDeepLink({ url } as Linking.EventType)
+			}
+		})
+
+		// For in-app events
+		const sub = Linking.addEventListener('url', handleDeepLink)
+		return () => sub.remove()
+	}, [])
+
+	return (
+		<>
+			<StatusBar style={colorScheme === 'dark' ? 'light' : 'dark'} />
+			<Stack screenOptions={{ headerShown: false }} />
+		</>
+	)
+}
---
+++ examples/with-expo/babel.config.js
@@ -0,0 +1,14 @@
+module.exports = function (api) {
+	api.cache(true)
+	return {
+		presets: [
+			[
+				'babel-preset-expo',
+				{
+					unstable_transformImportMeta: true,
+				},
+			],
+		],
+		plugins: ['expo-router/babel'],
+	}
+}
